71738efa1ecafbc7a8137d7fba6709e74307e689
unknown <Mohamed Sayed Gharib>
2023-06-13 09:07:31 +0300

    feat: add my source code

---
+++ sql/V1__Create_user_table.sql
@@ -0,0 +1,11 @@
+create table user (
+    ID int not null,
+    NAME varchar(100) not null
+    EMAIL varchar(100) not null
+    PASSWORD varchar(100) not null
+    PASSWORDCONFIRM varchar(100) not null
+    PHONE varchar(14)
+    
+
+
+);
